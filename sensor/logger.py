5324f788b6a86223417caaacdb4e36e5e5fba16a
prabhat1099 <prabhatkumars48@gmail.com>
2023-01-22 10:35:55 +0530

    logger updated

--- sensor/logger.py
+++ sensor/logger.py
@@ -7,7 +7,7 @@ LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
 LOG_FILE_DIR=os.path.join(os.getcwd(),"logs")
 #create folder if not available
 os.makedirs(LOG_FILE_DIR,exist_ok=True)
-#log file path
+#log file path.
 LOG_FILE_PATH=os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
 
 logging.basicConfig(
