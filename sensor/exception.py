8af3add9445f340d0e348491e11e5154744ff4ea
prabhat1099 <prabhatkumars48@gmail.com>
2023-01-22 22:57:49 +0530

    exception file added

--- sensor/exception.py
+++ sensor/exception.py
@@ -9,7 +9,7 @@ def error_message_detail(error, error_detail: sys):
     )
     return error_message
 
-class SensorException(Exception):
+class SensorException(Exceptioqn):
     def __init__(self, error_message, error_detail:sys):
         """
         :param error_message: error message in string format
